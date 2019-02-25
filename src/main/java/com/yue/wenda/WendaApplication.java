f93fa2521918d9627c327270307a1e48e7103909
Alan Fang <yuewake@hotmail.com>
2019-02-25 16:48:01 +0800

    第一次上传

---
+++ src/main/java/com/yue/wenda/WendaApplication.java
@@ -0,0 +1,13 @@
+package com.yue.wenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WendaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yue/wenda/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.yue.wenda.controller;
+
+import com.yue.wenda.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+
+}
