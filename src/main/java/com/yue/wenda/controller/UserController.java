a8d3ca44d40550fc5e3f0ebdef178424c53c3a45
Alan Fang <yuewake@hotmail.com>
2019-02-26 15:46:08 +0800

    添加了登陆 注册页面

--- src/main/java/com/yue/wenda/controller/UserController.java
+++ src/main/java/com/yue/wenda/controller/UserController.java
@@ -1,15 +1,44 @@
 package com.yue.wenda.controller;
 
 import com.yue.wenda.model.User;
+import org.apache.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class UserController {
 
+    /**
+     * 跳转到注册页面
+     * @return
+     */
+    @RequestMapping("/toRegist")
+    public String toRegist(){
+        return "regist";
+    }
 
+    /**
+     * 跳转到登陆页面
+     */
+    @RequestMapping("/toLogin")
+    public  String toLogin(){
+        return "login";
+    }
+
+    /**
+     * 进行登陆
+     * @return
+     */
+    @RequestMapping("login")
+    public String login(HttpServletRequest request){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println(username + ":" + password);
+        return "index";
+    }
 }
